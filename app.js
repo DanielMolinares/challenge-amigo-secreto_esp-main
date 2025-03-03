f727b904e24af419077c016509dae31ae27191f4
daniel <daniel.molinares0834@alumnos.udg.mx>
2025-03-02 23:22:34 -0600

    if para verificar si amigo ya está en la lista, función de renderizar amigo, función de sortear amigo

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-let amigos = []
+let amigo = [];
 
 function agregarAmigo() {
     let inputAmigo = document.getElementById('amigo');
-    let amigo = inputAmigo.value;
+    let nombreAmigo = inputAmigo.value;
 
-    if (!amigo) {
+    if (!nombreAmigo) {
         alert('Debes ingresar un nombre');
         return;
     }
+    //if extra para verifica si el amigo ya está en la lista
+    if (amigo.includes(nombreAmigo)) {
+        alert('El amigo ya está en la lista');
+        return;
+    }
+    amigo.push(nombreAmigo);
+    inputAmigo.value = '';
+    inputAmigo.focus();
+    renderizarAmigos();
+}
 
+function renderizarAmigos(){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    for (let i = 0; i < amigo.length; i++){
+        let item = document.createElement('li');
+        item.textContent = amigo[i];
+        listaAmigos.appendChild(item);
+    }
+}
+
+function sortearAmigo(){
+    if(amigo.length === 0){
+        alert('No hay amigos en la lista');
+        return;
+    }
+    let amigoSorteado = amigo[Math.floor(Math.random() * amigo.length)];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `${amigoSorteado} es el amigo secreto`;
 }
